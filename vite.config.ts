82925cb98e0c11919f86b348d77be9f198ac7f04
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2025-04-26 17:45:15 -0300

    feat: Integrate PWA support and enhance application configuration
    - Added PWA support using vite-plugin-pwa, enabling offline capabilities and improved user experience.
    - Introduced a manifest.json file for better web app integration, including app name, theme color, and icons.
    - Updated .gitignore to include dev-dist directory.
    - Modified index.html to link the new manifest and set the theme color.
    
    These changes enhance the application's performance and accessibility as a progressive web app.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+    }
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
