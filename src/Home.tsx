4b40ee0696001ca344dde1e49aa059bec4801c96
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2025-04-26 18:22:47 -0300

    Replace placeholder content in Home component with navigation to transactions page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,11 @@
+import { Navigate } from "react-router";
+
 function Home() {
   return (
     <div className="flex h-screen">
       <div className="flex-1 flex flex-col">
         <main className="flex-1 p-6 overflow-auto">
-          <div className="flex flex-col gap-8">blabla</div>
+          <Navigate to="/transactions" />
         </main>
       </div>
     </div>
